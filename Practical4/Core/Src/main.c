b877ffa1e63ee714738f680e6cd69a059e0451b0
segcboi <bhyebr002@myuct.ac.za>
2025-10-02 14:47:00 +0200

    task 3

--- Practical4/Core/Src/main.c
+++ Practical4/Core/Src/main.c
@@ -34,9 +34,9 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 // TODO: Add values for below variables
-#define NS = 128;       // Number of samples in LUT
-#define TIM2CLK = 16e6; // STM Clock frequency: Hint You might want to check the ioc file
-#define F_SIGNAL = 125e3;	// Frequency of output analog signal
+#define NS  128       // Number of samples in LUT
+#define TIM2CLK 16e6 // STM Clock frequency: Hint You might want to check the ioc file
+#define F_SIGNAL  125e3	// Frequency of output analog signal
 
 /* USER CODE END PD */
 
@@ -177,7 +177,7 @@ uint32_t Drum_LUT[] = {
 
 
 // TODO: Equation to calculate TIM2_Ticks
-uint32_t TIM2_Ticks = 0; // How often to write new LUT value
+uint32_t TIM2_Ticks = TIM2CLK / (F_SIGNAL * NS); // How often to write new LUT value
 uint32_t DestAddress = (uint32_t) &(TIM3->CCR3); // Write LUT TO TIM3->CCR3 to modify PWM duty cycle
 
 
